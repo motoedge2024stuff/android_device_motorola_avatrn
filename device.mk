97077124f0006a09a7711083c5d04cfa811f8f41
luna <115525658+lunarriaa@users.noreply.github.com>
2025-01-06 10:13:54 -0500

    Update device.mk

--- device.mk
+++ device.mk
@@ -21,7 +21,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     bootctrl.parrot
 
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
+# PRODUCT_STATIC_BOOT_CONTROL_HAL := \
     bootctrl.parrot \
     libgptutils \
     libz \
